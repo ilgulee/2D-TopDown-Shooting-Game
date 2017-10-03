5d44bfe9939afc286870489c2140c05b902142c7
ilgulee <iglee2000ca@gmail.com>
2017-10-02 21:49:05 -0400

    Initial commit

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.LeftArrow))  //if you want to check whether a key has been released, use GetKeyDown instead
+	    {
+	        gameObject.transform.Translate(Vector3.left*0.1f); //o.1m to the left
+	    }
+	    if (Input.GetKey(KeyCode.RightArrow))
+	    {
+	        gameObject.transform.Translate(Vector3.right * 0.1f);
+	    }
+	    if (Input.GetKey(KeyCode.UpArrow))
+	    {
+	        gameObject.transform.Translate(Vector3.up * 0.1f);
+	    }
+	    if (Input.GetKey(KeyCode.DownArrow))
+	    {
+	        gameObject.transform.Translate(Vector3.down * 0.1f);
+	    }
+    }
+}
+//CTRL+P to stop game scene

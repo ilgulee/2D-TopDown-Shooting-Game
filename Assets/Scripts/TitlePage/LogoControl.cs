69cfff03de0864ec92161a5cf213999efd5af380
Chamsol Yoon <hkarling@users.noreply.github.com>
2017-12-12 10:05:51 -0500

    changes

---
+++ Assets/Scripts/TitlePage/LogoControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoControl : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (transform.rotation.x > 0)
+            transform.Rotate(-Time.deltaTime * 30, 0, 0);
+        else
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+	}
+}
---
+++ Assets/Scripts/TitlePage/TitleControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleControl : MonoBehaviour {
+
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = GetComponent<RectTransform>().localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x += 5f;
+        if (transform.localPosition.x < -100)
+            GetComponent<RectTransform>().localPosition = offset;
+    }
+}

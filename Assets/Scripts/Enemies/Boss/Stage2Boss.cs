84cb54706c76d34d05bb94a73b89a0fb633e0f44
Chamsol Yoon <14797257+hkarling@users.noreply.github.com>
2017-12-19 19:01:38 -0500

    basic movement

--- Assets/Scripts/Enemies/Boss/Stage2Boss.cs
+++ Assets/Scripts/Enemies/Boss/Stage2Boss.cs
@@ -7,10 +7,12 @@ namespace Assets.Scripts
     public class Stage2Boss : MonoBehaviour
     {
         bool incoming;
+        Vector3 posStarting;
         // Use this for initialization
         void Start()
         {
             incoming = true;
+            posStarting = new Vector3(0, 3.5f, 0);
             GetComponent<Rigidbody2D>().velocity = Vector2.down;
         }
 
@@ -22,13 +24,25 @@ namespace Assets.Scripts
 
         void Movement()
         {
+            // reset movement
             if (incoming && GetComponent<Transform>().position.y <= 3.5f)
             {
-                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 GetComponent<Transform>().position = new Vector3(0, 3.5f, 0);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(1, 1);
                 incoming = false;
             }
+            else if(!incoming)
+            {
+                if(GetComponent<Transform>().position.x >= 2)
+                    GetComponent<Rigidbody2D>().velocity += new Vector2(-1, 0);
+                else if(GetComponent<Transform>().position.x <= -2)
+                    GetComponent<Rigidbody2D>().velocity += new Vector2(1, 0);
 
+                if (GetComponent<Transform>().position.y >= 4.5f)
+                    GetComponent<Rigidbody2D>().velocity += new Vector2(0, -1);
+                else if (GetComponent<Transform>().position.y <= 2.5f)
+                    GetComponent<Rigidbody2D>().velocity += new Vector2(0, 1);
+            }
         }
     }
 }
